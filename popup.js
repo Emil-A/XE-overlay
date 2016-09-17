427c4147f5463ec255901c0afe19d324a8d1c62a
Snuupy <snuupy[-removethis-]@snusite.com>
2016-09-17 13:28:18 -0400

    fixing ajax request and console logging

--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
-
-chrome.extension.getBackgroundPage().console.log('foo');
+chrome.extension.getBackgroundPage().console.log('extension opened');
 
 function checkCurrency(text) {
   var matches = text.match(/\d+/g);
@@ -11,7 +10,7 @@ function checkCurrency(text) {
       var currenciesList = JSON.parse(xhr.responseText).currencies;
       for (var i = 0; i < currenciesList.length; i++) {
         var currency = currenciesList[i];
-        if ((text.indexOf(currency.iso) !== -1 || text.indexOf(currency.currency_name) !== -1) && !currency.obsolete) { 
+        if ((text.indexOf(currency.iso) !== -1 || text.indexOf(currency.currency_name) !== -1) && !currency.obsolete) {
           //Currency symbol in text, scrape all digits 
           var amount = text.match(/\d/g);
           amount = amount.join("");
@@ -25,29 +24,37 @@ function checkCurrency(text) {
 }
 
 function getExData(currency, amount) {
+  console.log("fetching EX data");
   var xhr = new XMLHttpRequest();
 
   //xhr.open(method, url, true)
   user = "hackthenorth067";
   pass = "Waterloo1756";
-  xhr.open("GET", "https://xecdapi.xe.com/v1/convert_from.json/?from="+currency+"&to=CAD&amount="+amount, true);
+  xhr.open("GET", "https://xecdapi.xe.com/v1/convert_from.json/?from=" + currency + "&to=CAD&amount=" + amount, true);
   xhr.withCredentials = true;
-  xhr.setRequestHeader("Authorization", "Basic " + btoa(user+":"+pass));
-  
+  xhr.setRequestHeader("Authorization", "Basic " + btoa(user + ":" + pass));
 
   xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      var resp = JSON.parse(xhr.responseText);
-      console.log(resp.to[0].mid); //this
-      chrome.extension.getBackgroundPage().console.log(resp.to.mid); //this
-    } else {
-      alert("Request Failed. Check console for details.");
-      var resp = JSON.parse(xhr.responseText);
-      chrome.extension.getBackgroundPage().console.log(resp.message);
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        var resp = JSON.parse(xhr.responseText);
+        chrome.extension.getBackgroundPage().console.log(xhr.response);
+        chrome.extension.getBackgroundPage().console.log(resp.to[0].mid);
+        chrome.extension.getBackgroundPage().console.log(resp.to.mid);
+
+      } else {
+        alert("Request Failed. Check console for details.");
+        var resp = JSON.parse(xhr.responseText);
+        chrome.extension.getBackgroundPage().console.log(resp);
+        chrome.extension.getBackgroundPage().console.log(resp.message);
+      }
+
     }
   }
-  xhr.send();
-  console.log(xhr.response);
+  console.log("sending xhr request");
+  console.log(xhr.send());
+  // xhr.send();
+  // console.log(xhr.response);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -55,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   button.addEventListener('click', function() {
     checkCurrency("USD 100");
-    //getExData("USD", "100");
+    // getExData("USD", "100");
   });
 
 

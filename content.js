22d28bafd6c86ea1710904e70efb68f72b09e8ee
Snuupy <snuupy[-removethis-]@snusite.com>
2016-09-18 04:05:32 -0400

    updating content.js

--- content.js
+++ content.js
@@ -18,40 +18,12 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	overlay.style.backgroundColor = "#ADD8E6";
 	overlay.style.borderRadius = "10px";
 	overlay.innerHTML = exchangeinfo.originalamount + " " + exchangeinfo.originaliso + " = " + exchangeinfo.convertedamount + " " + exchangeinfo.convertediso;
+	overlay.innerHTML
 	document.body.appendChild(overlay);
 	overlay.appendChild(image);
 
 
-
-	//insert button to DOM
-	// console.log('inserting image to DOM');
-	// var button = document.createElement('img');
-	// button.setAttribute("src", "images/38x38.png");
-	// button.id = 'currencyOverlay';
-	// button.style.position = 'absolute';
-	// button.style.left = x + 5 + 'px';
-	// button.style.top = y + 5 + 'px';
-	// document.body.appendChild(button);
-	// console.log(message); //it works fam
-	// // chrome.runtime.sendMessage({type: "submit"});
-
-	// // var button = document.getElementById('currencyOverlay');
-
-	// button.onclick = function() {
-	// 	var button = document.getElementById('currencyOverlay');
-	// 	button.parentNode.removeChild(button);
-
-	// 	var div = document.createElement('div');
-	// 	div.id = 'currencyOverlay2';
-	// 	div.style.left = x + 5 + 'px';
-	// 	div.style.top = y + 5 + 'px';
-	// 	div.innerHTML = "test";
-	// 	// exchangeinfo.originalamount + " " + exchangeinfo.originaliso + " = " exchangeinfo.convertedamount + " " + exchangeinfo.convertediso;
-	// }
-
-
 });
-// var button = document.getElementById('currencyOverlay');
 
 
 document.onmouseup = function(e) {
